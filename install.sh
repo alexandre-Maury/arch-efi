d5ad5156d84f12dd71a84de3fa0a5b5cc5988225
alexandre-Maury <alexandre_maury@outlook.com>
2024-12-28 15:39:59 +0100

    maj

--- install.sh
+++ install.sh
@@ -14,17 +14,14 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # quel que soit le répertoire à partir duquel il est exécuté.
 
 source $SCRIPT_DIR/config/config.sh
-# Charge le fichier de configuration situé dans le sous-dossier misc/config.
-# Ce fichier est censé contenir des variables de configuration personnalisables pour le script.
+# Charge le fichier de configuration situé dans le sous-dossier config.
 
 source $SCRIPT_DIR/functions/functions.sh  
 # Charge un fichier contenant des fonctions utilitaires génériques.
-# Ces fonctions peuvent être utilisées dans tout le script pour simplifier ou centraliser certaines opérations.
 
 source $SCRIPT_DIR/functions/functions_disk.sh  
 # Charge un fichier contenant des fonctions spécifiques à la gestion des disques.
-# Par exemple, cela peut inclure des fonctions pour le partitionnement, le formatage, etc.
 
 source $SCRIPT_DIR/functions/functions_install.sh  
 # Charge un fichier contenant des fonctions dédiées à l'installation du système.
-# Cela peut inclure des étapes comme l'installation des paquets, la configuration des services, etc.
+

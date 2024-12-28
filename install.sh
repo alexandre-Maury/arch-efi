0b5f12c328fe52d6b2930ff77f46fa3a9a597b94
alexandre-Maury <alexandre_maury@outlook.com>
2024-12-28 15:51:11 +0100

    maj

--- install.sh
+++ install.sh
@@ -41,3 +41,38 @@ log_prompt "INFO" && echo "Vérification de la connexion Internet"
 $(ping -c 3 archlinux.org &>/dev/null) || (log_prompt "ERROR" && echo "Pas de connexion Internet" && echo)
 sleep 2
 
+##############################################################################
+## Récupération des disques disponibles                                                      
+##############################################################################
+list="$(lsblk -d -n | grep -v -e "loop" -e "sr" | awk '{print $1, $4}' | nl -s") ")" 
+
+if [[ -z "${list}" ]]; then
+    log_prompt "ERROR" && echo "Aucun disque disponible pour l'installation."
+    exit 1  # Arrête le script ou effectue une autre action en cas d'erreur
+else
+    clear
+    echo
+    log_prompt "INFO" && echo "Choisissez un disque pour l'installation (ex : 1) " && echo
+    echo "${list}" && echo
+fi
+
+# Boucle pour que l'utilisateur puisse choisir un disque ou en entrer un manuellement
+option=""
+while [[ -z "$(echo "${list}" | grep "  ${option})")" ]]; do
+    
+    log_prompt "INFO" && read -p "Votre Choix : " option 
+    
+    # Vérification si l'utilisateur a entré un numéro (choix dans la liste)
+    if [[ -n "$(echo "${list}" | grep "  ${option})")" ]]; then
+        # Si l'utilisateur a choisi un numéro valide, récupérer le nom du disque correspondant
+        disk="$(echo "${list}" | grep "  ${option})" | awk '{print $2}')"
+        break
+    else
+        # Si l'utilisateur a entré quelque chose qui n'est pas dans la liste, considérer que c'est un nom de disque
+        disk="${option}"
+        break
+    fi
+done
+
+clear
+
